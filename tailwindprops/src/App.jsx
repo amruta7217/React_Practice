6e9b2899e5ca30d6057cfb0c8e65228a6e3b34b5
Amruta Adsul <41994087+amruta7217@users.noreply.github.com>
2023-08-28 15:35:24 +0530

    Add files via upload
    Tailwind css with props

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card name="Amruta" btnText="Click Me"/>
+      <Card name="Pooja" />
+    </>
+  )
+}
+
+export default App
